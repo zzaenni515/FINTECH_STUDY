4888f8923c62f085539c41e8a8fd6a913ea1efb5
zzaenni515 <zzaenni515@naver.com>
2020-06-16 10:36:26 +0900

    first commit

---
+++ func.js
@@ -0,0 +1,20 @@
+function multi(p1, p2) {
+	return p1 * p2; // p1, p2 곱연산의 결과를 반환한다.
+}
+
+function plus(p1, p2) {  //더하기 연산
+    return p1 + p2;
+}
+
+function minus(p1, p2) {  // 마이너스 연산
+    return p1 - p2;
+}
+
+function div(p1, p2) {  //나누기 연산
+    return p1 / p2;
+}
+
+console.log(multi(8,9));
+console.log(plus(8,9));
+console.log(minus(8,9));
+console.log(div(8,9));
