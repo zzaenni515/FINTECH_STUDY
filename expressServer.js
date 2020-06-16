bb2ee5bd944a254756ac4307f19e2046a886935f
zzaenni515 <zzaenni515@naver.com>
2020-06-16 16:31:05 +0900

    express 프레임워크 설치 예제 코드 실행

---
+++ expressServer.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+ 
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+ 
+app.get('/test', function (req, res) {
+    res.send('안녕하세요 express 프레임 워크입니다.')
+  })
+
+app.listen(3000)
