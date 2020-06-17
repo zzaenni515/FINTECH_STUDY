3fe77385617b8d3896aa7f9c7f85b7cdf8699974
zzaenni515 <zzaenni515@naver.com>
2020-06-17 16:12:50 +0900

    부모창에 데이터 전송 예제

--- fintechServer.js
+++ fintechServer.js
@@ -40,7 +40,8 @@ app.get('/authResult', function(req, res){
     }
     request(option, function(error, response, body){
         console.log(body);
-        res.json(body);
+        var requestResultJSON = JSON.parse(body);
+        res.render('resultChild',{data : requestResultJSON})
     })
 })
 
