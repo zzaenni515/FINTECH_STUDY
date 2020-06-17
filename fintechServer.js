16ea90486caf334d96047f71ba38bece664b4977
zzaenni515 <zzaenni515@naver.com>
2020-06-17 11:40:11 +0900

    프로젝트 구성 기본 완료

---
+++ fintechServer.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const path = require('path');
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+app.use(express.static(path.join(__dirname, 'public')));//to use static asset
+
+app.get('/design', function(req, res){
+    res.render('wallet');
+})
+
+app.listen(3000)
