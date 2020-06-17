66104458359fb72dfafd2b482c079fdc7e86815d
zzaenni515 <zzaenni515@naver.com>
2020-06-17 11:03:36 +0900

    ajax 구성, 소스 정리

--- expressServer.js
+++ expressServer.js
@@ -4,6 +4,11 @@ const app = express()
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+app.use(express.static(path.join(__dirname, 'public')));//to use static asset
+
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
@@ -22,6 +27,20 @@ app.get('/test3', function (req, res) {
 
 app.get('/inputTest', function(req, res){
     res.render('inputTest');
+})
+
+//브라우저는 Get방식으로만 요청가능!
+
+app.post('/inputTest', function(req, res){
+  console.log('request!!');
+  //console.log(req.body);
+  console.log(req.body.userIdBody);
+  console.log(req.body.userPasswordBoby);
+  res.json(1);
+})
+
+app.post('/postTest', function(req, res){
+  res.render('post!!!!!!!');
 })
 
 app.listen(3000)
