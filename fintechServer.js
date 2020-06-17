ca1490a02c872ef85077e11665e9163c1e51d341
zzaenni515 <zzaenni515@naver.com>
2020-06-17 16:04:17 +0900

    authResult 완성

--- fintechServer.js
+++ fintechServer.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const path = require('path');
+const request = require('request');
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -17,6 +18,30 @@ app.get('/design', function(req, res){
 //회원가입페이지
 app.get('/signup', function(req, res){
     res.render('signup');
+})
+
+app.get('/authResult', function(req, res){
+    console.log('authResult');
+    console.log(req.query);
+    var authCode = req.query.code;
+    var option = {
+        method : "POST",
+        url : "https://testapi.openbanking.or.kr/oauth/2.0/token",
+        header : {
+            'Content-Type' : 'application/x-www-form-urlencoded'
+        },
+        form : {
+            code : authCode,
+            client_id : 'MTjFQJ2tkHnPNTWfOSi3p1oiIF1BZ9WE0I2HW6RQ',
+            client_secret : 'jHBnRMjW2V8qxUcoj5ES14kemzwIuELgu7z2wkfK',
+            redirect_uri : 'http://localhost:3000/authResult',
+            grant_type : 'authorization_code'
+        }
+    }
+    request(option, function(error, response, body){
+        console.log(body);
+        res.json(body);
+    })
 })
 
 app.listen(3000)
