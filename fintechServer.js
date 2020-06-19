1051b5846ac53e03fb6a76ee54ff2c0dae86e8cd
zzaenni515 <zzaenni515@naver.com>
2020-06-19 12:00:56 +0900

    결제 완료

--- fintechServer.js
+++ fintechServer.js
@@ -258,6 +258,61 @@ app.post('/transactionlist', auth, function (req, res) {
     })
 })
 
+
+app.post('/withdraw', auth, function (req, res) {
+    var userId = req.decoded.userId;
+    var fin_use_num = req.body.fin_use_num;
+    var countnum = Math.floor(Math.random() * 1000000000) + 1;
+    var transId = "T991637160U" + countnum; //이용기관번호 본인것 입력
+
+    var sql = "SELECT * FROM user WHERE id = ?"
+    connection.query(sql,[userId], function(err , result){
+        if(err){
+            console.error(err);
+            throw err
+        }
+        else {
+            console.log(result);
+            var option = {
+                method : "POST",
+                url : "https://testapi.openbanking.or.kr/v2.0/transfer/withdraw/fin_num",
+                headers : {
+                    Authorization : 'Bearer ' + result[0].accesstoken,
+                    "Content-Type" : "application/json"
+                },
+                json : {
+                    "bank_tran_id": transId,
+                    "cntr_account_type": "N",
+                    "cntr_account_num": "7199789104",
+                    "dps_print_content": "쇼핑몰환불",
+                    "fintech_use_num": "199163716057884778494509",
+                    "wd_print_content": "오픈뱅킹출금",
+                    "tran_amt": "1000",
+                    "tran_dtime": "20200424131111",
+                    "req_client_name": "홍길동",
+                    "req_client_fintech_use_num" : "199163716057884778494509",
+                    "req_client_num": "HONGGILDONG1234",
+                    "transfer_purpose": "TR",
+                    "recv_client_name": "황채린",
+                    "recv_client_bank_code": "097",
+                    "recv_client_account_num": "7199789104"
+                }
+            }
+            request(option, function(err, response, body){
+                if(err){
+                    console.error(err);
+                    throw err;
+                }
+                else {
+                    console.log(body);
+                    if(body.rsp_code == 'A0000'){
+                        res.json(1)
+                    }
+                }
+            })
+        }
+    })
+})
 // app.get('/authTest', auth, function(req, res){
 //     res.json(req.decoded);
 // })
